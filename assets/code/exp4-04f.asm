2741bcf0c9d7e9b992070f6db9adcfdc1a54a5e6
Patrick Young <deploy@travis-ci.org>
2018-07-29 03:52:04 +0000

    Deploy asm-learning-notes to github.com/kmahyyg/asm-learning-notes.git:gh-pages

---
+++ assets/code/exp4-04f.asm
@@ -0,0 +1,22 @@
+assume cs:code
+
+a segment
+  db 1,2,3,4,5,6,7,8
+a ends
+
+b segment
+  db 1,2,3,4,5,6,7,8
+b ends
+
+c segment 
+  db 0,0,0,0,0,0,0,0
+c ends
+
+code segment
+
+start:
+  ; fill in the blank here
+  
+code ends
+
+end start
