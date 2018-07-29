50f225b6d47fb4bda2424f981a815596d6cfd929
Patrick Young <deploy@travis-ci.org>
2018-07-29 08:03:54 +0000

    Deploy asm-learning-notes to github.com/kmahyyg/asm-learning-notes.git:gh-pages

--- assets/code/exp4-05f.asm
+++ assets/code/exp4-05f.asm
@@ -1,17 +1,30 @@
 assume cs:code 
 
-a segment
+dataa segment
   dw 1,2,3,4,5,6,7,8,9,0ah,0bh,0ch,0dh,0eh,0fx,0ffh
-a ends
+dataa ends
 
-b segment
+stackb segment
   dw 0,0,0,0,0,0,0,0
-b ends
+stackb ends
 
 code segment
 
-start:
-  ; fill in the blank here
+start: mov ax,stackb  ; set stack segment base addr
+       mov ss,ax     ; set stack segment base addr
+       mov sp,16     ; set the top flag of stack, stack max saved 16 bytes
+       
+       mov ax,dataa  ; set data segment base addr
+       mov ds,ax     ; set data segment base addr
+       
+       mov bx,0      ; set offset addr
+       mov cx,8      ; set loop times * 8
+       
+    s: push ds:[bx]  ; push data into stack
+       add bx,2      ; add offset 2
+       
+       mov ax,4c00h
+       int 21h
   
 code ends
 
