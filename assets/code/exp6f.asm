1c08f415ad8d46619d1fa76b4f74ca2880241d62
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-08-06 00:50:44 +0800

    correct exp6 code,finished without testing

--- assets/code/exp6f.asm
+++ assets/code/exp6f.asm
@@ -1,11 +1,22 @@
-;code untested
-;donnot use
-;by kmahyyg
-;licensed under agplV3
+; asm-learning-notes
+; Copyright (C) 2018  kmahyyg
+; 
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU Affero General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+; 
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU Affero General Public License for more details.
+; 
+; You should have received a copy of the GNU Affero General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-assume cs:codesg,ds:datasg,ss:table
+assume cs:codesg,ds:datasg,es:table
 
-datasg segment   ; seen as stack
+datasg segment   ; seen as data segment
 
     db '1975','1976','1977','1978','1995'    ;years
     dd 16,22,382,1456,5937000    ; total salary
@@ -13,7 +24,7 @@ datasg segment   ; seen as stack
     
 datasg ends
 
-table segment
+table segment    ; seen as extra data segment
 
     db 5 dup ('year total employees average ')
     
@@ -40,7 +51,7 @@ codesg segment
             mov es:[bx],ax
             mov al,[di+3]
             mov es:[bx],ax
-            ; years in table 4bytes
+            ; years in table 4 bytes (32 bits)
             
             ; remember data structure 
             mov ax,14h[di]     ; jump to the start of salary, 20 bytes offset
@@ -48,22 +59,22 @@ codesg segment
             mov es:5h[bx],ax     ; choose the salary data
             mov es:7h[bx],dx      ; write salary
             
-            mov ax,ss:[si+38]
-            mov [bx+10],ax     ; offset 10 for employees
+            mov ax,28h[si]      ; 40 bytes in datasg segment offset (4 bytes == 32 bits)
+            mov es:0ah[bx],ax   ; employees' numbers into table segment  
             
-            mov ax,[bx+5]
-            mov dx,[bx+7]
-            div word ptr [bx+10]
-            mov [bx+13],ax
+            mov ax,20h[di]      ; save num to be divided (2 bytes == 16 bits)
+            mov dx,22h[di]      ; which is salary
+            div word ptr 28h[si]   ; divided by 28h[si]
+            mov es:0dh[bx],ax   ; quotient saved to table segment
             
-            add bx,16
-            add di,4
-            add si,2
+            add bx,16  ; next line
+            add di,4   ; 4 bytes offset (other datas)
+            add si,2   ; 2 bytes offset (employees)
             
-            loop inpt
+            loop inpt  ; loop code
             
             mov ax,4c00h
-            int 21h
+            int 21h      ; back to normal
             
 codesg ends
 
